c686e52f3a4b4c1273b45a7cbf6e5c1cc161d93b
kerrian21 <144936685+kerrian21@users.noreply.github.com>
2023-11-28 16:49:58 +0200

    Update Fix_database.sql

--- backend/Fix_database.sql
+++ backend/Fix_database.sql
@@ -8,81 +8,81 @@ DROP TABLE IF EXISTS address;
 DROP TABLE IF EXISTS addition;
 
 CREATE TABLE products (
-product_id INT PRIMARY KEY,
-seller_id INT,
-title VARCHAR(20),
-price INT,
-rating INT,
-rating_count INT
+product_id INTEGER PRIMARY KEY AUTOINCREMENT ,
+seller_id INTEGER,
+title TEXT,
+price INTEGER,
+rating INTEGER,
+rating_count INTEGER
 );
 
 CREATE TABLE sells(
-sell_id INT PRIMARY KEY,
-product_id INT,
-seller_id INT,
-client_id INT,
-date_of_sale DATE,
-date_of_insertion DATE,
-status_id VARCHAR(15),
-items_count INT,
-address_id INT
+sell_id INTEGER PRIMARY KEY AUTOINCREMENT ,
+product_id INTEGER,
+seller_id INTEGER,
+client_id INTEGER,
+date_of_sale TEXT,
+date_of_insertion TEXT,
+status_id TEXT,
+items_count INTEGER,
+address_id INTEGER
 );
 
 CREATE TABLE seller(
-seller_id INT PRIMARY KEY,
-title VARCHAR(30),
-first_name VARCHAR (15),
-last_name VARCHAR (20),
-rating INT,
-registration_date DATE
+seller_id INTEGER PRIMARY KEY AUTOINCREMENT ,
+title TEXT,
+first_name TEXT,
+last_name TEXT,
+rating REAL,
+registration_date TEXT
 );
 
 CREATE TABLE user(
-user_id INT PRIMARY KEY,
-login VARCHAR(20) UNIQUE,
-password VARCHAR(30),
-phone INT,
-email VARCHAR(20)
+user_id INTEGER PRIMARY KEY AUTOINCREMENT ,
+login TEXT UNIQUE,
+password TEXT,
+phone INTEGER,
+email TEXT
 );
 
 CREATE TABLE addition(
-addition_id INT PRIMARY KEY,
-product_id INT,
-seller_id INT,
-items_count INT,
-size VARCHAR(4),
-date_of_adding DATE,
-is_written_of INT
+addition_id INTEGER PRIMARY KEY AUTOINCREMENT ,
+product_id INTEGER,
+seller_id INTEGER,
+items_count INTEGER,
+size TEXT,
+date_of_adding REAL,
+is_written_of INTEGER
 );
 
 CREATE TABLE address(
-address_id INT PRIMARY KEY,
-address VARCHAR(40)
+address_id INTEGER PRIMARY KEY AUTOINCREMENT ,
+address TEXT
 );
 
 CREATE TABLE product_sizes(
-size_id INT PRIMARY KEY,
-product_id INT,
-size VARCHAR(4),
-items_count INT
+size_id INTEGER PRIMARY KEY AUTOINCREMENT ,
+product_id INTEGER,
+size TEXT,
+items_count INTEGER
 );
 CREATE TABLE product_types(
-type_id INT PRIMARY KEY,
-description VARCHAR(200),
-product_id INT
+type_id INTEGER PRIMARY KEY AUTOINCREMENT ,
+description TEXT,
+product_id INTEGER
 );
 
 
 -- Ключ з size до продуктс(product_id)
 create table product_sizes_dg_tmp
 (
-    size_id     INT
-        primary key,
-    product_id  INT
+    size_id     INTEGER
+        primary key autoincrement,
+    product_id  INTEGER
         constraint product_sizes
             references products,
-    size        VARCHAR(4),
-    items_count INT
+    size        TEXT,
+    items_count INTEGER
 );
 
 insert into product_sizes_dg_tmp(size_id, product_id, size, items_count)
@@ -98,10 +98,10 @@ alter table product_sizes_dg_tmp
 -- Ключ з type до продуктс(product_id)
 create table product_types_dg_tmp
 (
-    type_id     INT
-        primary key,
-    description VARCHAR(200),
-    product_id  INT
+    type_id     INTEGER
+        primary key autoincrement,
+    description TEXT,
+    product_id  INTEGER
         constraint product_types
             references products
 );
@@ -116,20 +116,19 @@ alter table product_types_dg_tmp
     rename to product_types;
 
 
-
 -- Ключ з addition до продуктс(product_id)
 create table addition_dg_tmp
 (
-    addition_id    INT
-        primary key,
-    product_id     INT
+    addition_id    INTEGER
+        primary key autoincrement,
+    product_id     INTEGER
         constraint addition
             references products,
-    seller_id      INT,
-    items_count    INT,
-    size           VARCHAR(4),
-    date_of_adding DATE,
-    is_written_of  INT
+    seller_id      INTEGER,
+    items_count    INTEGER,
+    size           TEXT,
+    date_of_adding REAL,
+    is_written_of  INTEGER
 );
 
 insert into addition_dg_tmp(addition_id, product_id, seller_id, items_count, size, date_of_adding, is_written_of)
@@ -141,17 +140,19 @@ drop table addition;
 alter table addition_dg_tmp
     rename to addition;
 
+
+--Ключ з продуктс до seller(seller_id)
 create table products_dg_tmp
 (
-    product_id   INT
-        primary key,
-    seller_id    INT
+    product_id   INTEGER
+        primary key autoincrement,
+    seller_id    INTEGER
         constraint products
             references seller,
-    title        VARCHAR(20),
-    price        INT,
-    rating       INT,
-    rating_count INT
+    title        TEXT,
+    price        INTEGER,
+    rating       INTEGER,
+    rating_count INTEGER
 );
 
 insert into products_dg_tmp(product_id, seller_id, title, price, rating, rating_count)
