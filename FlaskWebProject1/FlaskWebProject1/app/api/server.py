364e4e3324fd8af8d11e2009127504e7b7285c9e
danbug555 <dadadank60@gmail.com>
2023-12-02 20:14:21 +0200

    new html files and update server file

--- FlaskWebProject1/FlaskWebProject1/app/api/server.py
+++ FlaskWebProject1/FlaskWebProject1/app/api/server.py
@@ -5,18 +5,9 @@ import sqlite3
 app = Flask(__name__)
 
 
-conn = sqlite3.connect('schema.sql')
+conn = sqlite3.connect('identifier.sqlite', check_same_thread=False)
 cursor = conn.cursor()
 
-
-
-def search_database(query):
-    cursor.execute('''
-        SELECT name, color, name_designer FROM T-Shorts, Footwear
-        WHERE name LIKE ? OR color LIKE ? OR name_designer LIKE ?
-    ''', ('%' + query + '%', '%' + query + '%', '%' + query + '%' ))
-    results = cursor.fetchall()
-    return results
 
 
 def check_user_exists(username, email, phone_number, password):
@@ -59,26 +50,41 @@ def register():
 
 @app.route('/search', methods=['POST','GET'])
 def search():
-   if request.methods == 'POST':
+   if request.method == 'POST':
         query = request.form['query']
-        results = search_database(query)
+        cursor.execute('SELECT title, price FROM products WHERE title = ?', [query])
+        results = cursor.fetchall()
+        print(results)
         return render_template('search_results.html', results=results)
 
 @app.route('/catalog', methods=['POST','GET'])
 def catalog():
-        if request.methods == 'POST':
-                category = request.form['Category']
-                name = request.form['Name']
-                years = request.form['Years']
-                color = request.form['Color']
-                prace = request.form['Prace']
+        if request.method == 'POST':
+            name = request.form['clothes']
+            if name == 'Shirt':
+                cursor.execute("SELECT title, price FROM products WHERE title = 'Moody Shirt'")
+                result = cursor.fetchall()
+                return render_template('shirt.html', result=result)
 
-                conn = sqlite3.connect('schema.sql')
-                cursor = conn.cursor()
-                cursor.execute("SELECT * FROM products WHERE types = ? name = ? Color = ? Prace = ? Years = ?", (category, name, color, prace, years))
-                result = cursor.fetchall() 
-                conn.close()
-                return render_template('t-short.html', result=result)
+            elif name == 't-shirt':
+                    cursor.execute("SELECT title, price FROM products WHERE title = 'T-Shirt lalala'")
+                    result = cursor.fetchall()
+                    return render_template('t-shirt.html', result=result)
+            elif name == 'Jacket':
+                    cursor.execute("SELECT title, price FROM products WHERE title IN ('Example Jacket', 'Jack')")
+                    result = cursor.fetchall()
+                    return render_template('jacket.html', result=result)
+            elif name == 'pants':
+                    cursor.execute("SELECT title, price FROM products WHERE title IN ('White pants', 'Black pants')")
+                    result = cursor.fetchall()
+                    return render_template('pants.html', result=result)
+            elif name == 'Footwear':
+                    cursor.execute("SELECT title, price FROM products WHERE title = 'FootweaRR'")
+                    result = cursor.fetchall()
+                    return render_template('footwear.html', result=result)
+
+
+
                
 @app.route('/range', methods=['POST','GET'])
 def _range_():
