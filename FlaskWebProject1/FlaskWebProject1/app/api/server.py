5accfb248c37a279081b33550fb9152718573a06
danbug555 <dadadank60@gmail.com>
2023-12-03 23:31:00 +0200

    new(2)

--- FlaskWebProject1/FlaskWebProject1/app/api/server.py
+++ FlaskWebProject1/FlaskWebProject1/app/api/server.py
@@ -24,7 +24,7 @@ def check_user_exists(username, email, phone_number, password):
 
 @app.route('/')
 def index():
-    cursor.execute('SELECT title, price FROM products ORDER BY price ASC')
+    cursor.execute('SELECT title, color, price FROM products ORDER BY price ASC')
     result = cursor.fetchall()
     return render_template('index.html', result=result)
 
@@ -52,46 +52,55 @@ def register():
 
 @app.route('/search', methods=['POST','GET'])
 def search():
-        if request.method == 'POST':
-            query = request.form.get('query')
-            parts = query.split()
+    if request.method == 'POST':
+        query = request.form.get('query')
+        parts = query.split()
 
-            title_query = ' '.join(parts)
-            title_sql_query = 'SELECT title, price FROM products WHERE title LIKE ? COLLATE NOCASE'
-            title_params = (f'%{title_query}%',)
-            cursor.execute(title_sql_query, title_params)
-            title_results = cursor.fetchall()
+        title_query = ' '.join(parts)
+        title_sql_query = 'SELECT title, color, price FROM products WHERE title LIKE ? COLLATE NOCASE'
+        title_params = (f'%{title_query}%',)
+        cursor.execute(title_sql_query, title_params)
+        title_results = cursor.fetchall()
 
-            color_query = parts[-1]
-            color_sql_query = 'SELECT title, price FROM products WHERE color LIKE ? COLLATE NOCASE'
-            color_params = (f'%{color_query}%',)
-            cursor.execute(color_sql_query, color_params)
-            color_results = cursor.fetchall()
+        color_query = parts[-1]
+        color_sql_query = 'SELECT title, color, price FROM products WHERE color LIKE ? COLLATE NOCASE'
+        color_params = (f'%{color_query}%',)
+        cursor.execute(color_sql_query, color_params)
+        color_results = cursor.fetchall()
 
-            combined_sql_query = 'SELECT title, price FROM products WHERE title LIKE ? COLLATE NOCASE AND color LIKE ? COLLATE NOCASE'
-            combined_params = (f'%{title_query}%', f'%{color_query}%')
-            cursor.execute(combined_sql_query, combined_params)
-            combined_results = cursor.fetchall()
+        price_sql_query = 'SELECT title, color, price FROM products WHERE price LIKE ? COLLATE NOCASE'
+        price_params = (f'%{query}%',)
+        cursor.execute(price_sql_query, price_params)
+        price_results = cursor.fetchall()
 
+        combined_sql_query = 'SELECT title, color, price FROM products WHERE title LIKE ? COLLATE NOCASE AND color LIKE ? COLLATE NOCASE AND price LIKE ? COLLATE NOCASE'
+        combined_params = (f'%{title_query}%', f'%{color_query}%', f'%{query}%')
+        cursor.execute(combined_sql_query, combined_params)
+        combined_results = cursor.fetchall()
 
-            if len(parts) >= 2:
-                multi_title_query = ' '.join(parts[:-1])
-                multi_title_sql_query = 'SELECT title, price FROM products WHERE title LIKE ? COLLATE NOCASE'
-                multi_title_params = (f'%{multi_title_query}%',)
-                cursor.execute(multi_title_sql_query, multi_title_params)
-                multi_title_results = cursor.fetchall()
-            else:
-                multi_title_results = []
+        if len(parts) >= 2:
+            multi_title_query = ' '.join(parts)
+            multi_title_sql_query = 'SELECT title, color, price FROM products WHERE title LIKE ? COLLATE NOCASE'
+            multi_title_params = (f'%{multi_title_query}%',)
+            cursor.execute(multi_title_sql_query, multi_title_params)
+            multi_title_results = cursor.fetchall()
+        else:
+            multi_title_results = []
 
-            all_results = title_results + color_results + combined_results + multi_title_results
-            unique_results = []
-            unique_titles_set = set()
-            for title, price in all_results:
-                title_lower = title.lower()
-                if title_lower not in unique_titles_set:
-                    unique_titles_set.add(title_lower)
-                    unique_results.append((title, price))
-            return render_template('search_results.html', results=unique_results)
+        all_results = title_results + color_results + price_results + combined_results + multi_title_results
+        unique_results = []
+        unique_titles_set = set()
+        for result in all_results:
+            title, color, price = result[:3] + (None,) * (3 - len(result))
+
+            title_lower = title.lower()
+
+            if title_lower not in unique_titles_set:
+                unique_titles_set.add(title_lower)
+                unique_results.append({'title': title, 'color': color, 'price': price})
+
+        return render_template('search_results.html', results=unique_results)
+
 
 
 @app.route('/catalog', methods=['POST','GET'])
@@ -99,33 +108,33 @@ def catalog():
         if request.method == 'POST':
             name = request.form['clothes']
             if name == 'Shirt':
-                cursor.execute("""SELECT title, price FROM products 
-                                        JOIN product_types ON products.product_id = product_types.product_id WHERE product_types.description = ?""",
+                cursor.execute("""SELECT title, color, price FROM products 
+                                        JOIN types ON products.product_id = types.product_id WHERE types.description = ?""",
                             (name,))
                 result = cursor.fetchall()
                 return render_template('shirt.html', result=result)
 
             elif name == 'T-Shirt':
-                    cursor.execute("""SELECT title, price FROM products 
-                                            JOIN product_types ON products.product_id = product_types.product_id WHERE product_types.description = ?""",
+                    cursor.execute("""SELECT title, color, price FROM products 
+                                            JOIN types ON products.product_id = types.product_id WHERE types.description = ?""",
                                 (name,))
                     result = cursor.fetchall()
                     return render_template('t-shirt.html', result=result)
             elif name == 'Jacket':
-                    cursor.execute("""SELECT title, price FROM products 
-                                            JOIN product_types ON products.product_id = product_types.product_id WHERE product_types.description = ?""",
+                    cursor.execute("""SELECT title, color, price FROM products 
+                                            JOIN types ON products.product_id = types.product_id WHERE types.description = ?""",
                                 (name,))
                     result = cursor.fetchall()
                     return render_template('jacket.html', result=result)
             elif name == 'Pants':
-                    cursor.execute("""SELECT title, price FROM products 
-                                           JOIN product_types ON products.product_id = product_types.product_id WHERE product_types.description = ?""",
+                    cursor.execute("""SELECT title, color, price FROM products 
+                                           JOIN types ON products.product_id = types.product_id WHERE types.description = ?""",
                                (name,))
                     result = cursor.fetchall()
                     return render_template('pants.html', result=result)
             elif name == 'Footwear':
-                    cursor.execute("""SELECT title, price FROM products 
-                                           JOIN product_types ON products.product_id = product_types.product_id WHERE product_types.description = ?""",
+                    cursor.execute("""SELECT title, color, price FROM products 
+                                           JOIN types ON products.product_id = types.product_id WHERE types.description = ?""",
                                (name,))
                     result = cursor.fetchall()
                     return render_template('footwear.html', result=result)
@@ -161,14 +170,14 @@ def filter_price():
         name = request.args.get('name')
 
         if name:
-            cursor.execute('SELECT title, price FROM products WHERE price = ?', (name,))
+            cursor.execute('SELECT title, color, price FROM products WHERE price = ?', (name,))
             exact_result = cursor.fetchall()
 
             if exact_result:
                 message = f"Here is what was found for your query '{name}'."
                 return render_template('results.html', message=message, results=exact_result)
             else:
-                cursor.execute('SELECT title, price FROM products WHERE price LIKE ? COLLATE NOCASE ORDER BY price ASC', ('%' + name + '%',))
+                cursor.execute('SELECT title, color, price FROM products WHERE price LIKE ? COLLATE NOCASE ORDER BY price ASC', ('%' + name + '%',))
                 similar_result = cursor.fetchall()
 
                 if similar_result:
